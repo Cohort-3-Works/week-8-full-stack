93e1ad841fe17617d0c3c5a6e3bf215ed735de6e
Subhajit Chaudhury <subhajitchaudhury05@gmail.com>
2024-09-26 03:02:30 +0530

    shifted to .env

--- routes/admin.js
+++ routes/admin.js
@@ -1,89 +1,128 @@
 const { Router } = require("express");
+require("dotenv").config();
 const { adminAuth } = require("../middleware/admin");
-const JWT_ADMIN_PASSWORD = require("../config");
+const { admin } = require("../db");
+const jwt = require("jsonwebtoken");
 const bcrypt = require("bcrypt");
 const adminRouter = Router();
 
 adminRouter.post("/signup", async function (req, res) {
-  const { email, password, firstName, lastName } = req.body;
+  try {
+    const { email, password, firstName, lastName } = req.body;
+    if (!email || !password || !firstName || !lastName) {
+      return res.status(400).json({
+        message: "Please provide all fields",
+      });
+    }
 
-  if (!email || !password || !firstName || !lastName) {
-    return res.status(400).json({
-      message: "Please provide all fields",
+    const existingUser = await admin.findOne({ email });
+    if (existingUser) {
+      return res.status(400).json({
+        message: "Admin already exists try using a different mail",
+      });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const newAdmin = new admin({
+      email,
+      password: hashedPassword,
+      firstName,
+      lastName,
+    });
+
+    await newAdmin.save();
+
+    return res.json({
+      success: true,
+      message: "Admin created successfully",
+    });
+  } catch (err) {
+    return res.json({
+      message: "Something went wrong !!!",
     });
   }
-
-  const existingUser = await admin.findOne({ email });
-  if (existingUser) {
-    return res.status(400).json({
-      message: "Admin already exists try using a different mail",
-    });
-  }
-
-  const hashedPassword = await bcrypt.hash(password, 10);
-
-  const newAdmin = new admin({
-    email,
-    password: hashedPassword,
-    firstName,
-    lastName,
-  });
-
-  await newAdmin.save();
-
-  return res.json({
-    success: true,
-    message: "Admin created successfully",
-  });
 });
 
 adminRouter.post("/signin", async function (req, res) {
-  const { email, password } = req.body;
+  try {
+    const { email, password } = req.body;
 
-  if (!email || !password) {
-    return res.status(400).json({
-      message: "Please provide all fields",
+    if (!email || !password) {
+      return res.status(400).json({
+        message: "Please provide all fields",
+      });
+    }
+    const existingUser = await admin.findOne({ email: email });
+
+    if (!existingUser) {
+      return res.status(400).json({
+        message: "Admin does not exist",
+      });
+    }
+
+    const isPasswordCorrect = await bcrypt.compare(
+      password,
+      existingUser.password
+    );
+    if (!isPasswordCorrect) {
+      return res.status(400).json({
+        message: "Incorrect password",
+      });
+    }
+
+    const token = jwt.sign(
+      {
+        id: existingUser._id,
+        email: existingUser.email,
+        firstName: existingUser.firstName,
+        lastName: existingUser.lastName,
+      },
+      process.env.JWT_ADMIN_PASSWORD
+    );
+
+    return res.json({
+      success: true,
+      message: "signed in successfully",
+      token,
+    });
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json({
+      success: false,
+      message: "Something went wrong !!!",
     });
   }
-  const existingUser = await admin.findOne({ email: email });
-
-  if (!existingUser) {
-    return res.status(400).json({
-      message: "Admin does not exist",
-    });
-  }
-
-  const isPasswordCorrect = await bcrypt.compare(
-    password,
-    existingUser.password
-  );
-  if (!isPasswordCorrect) {
-    return res.status(400).json({
-      message: "Incorrect password",
-    });
-  }
-
-  const token = jwt.sign(
-    {
-      id: existingUser._id,
-      email: existingUser.email,
-      firstName: existingUser.firstName,
-      lastName: existingUser.lastName,
-    },
-    JWT_ADMIN_PASSWORD
-  );
-
-  return res.json({
-    success: true,
-    message: "signed in successfully",
-    token,
-  });
 });
 
-adminRouter.post("/createcourse", function (req, res) {
-  res.json({
-    message: "signup endpoint",
-  });
+adminRouter.post("/createcourse", adminAuth, async function (req, res) {
+  try {
+    const { title, description, price, imageUrl } = req.body;
+    if (!title || !description || !price || !imageUrl) {
+      return res.status(400).json({
+        message: "Please provide all fields",
+      });
+    }
+    const newCourse = new course({
+      title,
+      description,
+      price,
+      imageUrl,
+      creatorId: req.id,
+    });
+
+    await newCourse.save();
+
+    return res.json({
+      success: true,
+      message: "Course created successfully",
+    });
+  } catch (err) {
+    return res.status(500).json({
+      success: false,
+      message: "Something went wrong !!!",
+    });
+  }
 });
 
 adminRouter.put("/deletecourse", function (req, res) {
--- routes/user.js
+++ routes/user.js
@@ -1,25 +1,120 @@
 const { Router } = require("express");
+require("dotenv").config();
+const bcrypt = require("bcrypt");
+const { user } = require("../db.js");
+const { userAuth } = require("../middleware/user.js");
+const jwt = require("jsonwebtoken");
 
 const userRouter = Router();
 
-userRouter.post("/signup", function(req, res) {
-    res.json({
-        message: "signup endpoint"
-    })
-})
+userRouter.post("/signup", async function (req, res) {
+  try {
+    const { email, password, firstName, lastName } = req.body;
 
-userRouter.post("/signin", function(req, res) {
-    res.json({
-        message: "signup endpoint"
-    })
-})
+    if (!email || !password || !firstName || !lastName) {
+      return res.status(400).json({
+        message: "Please provide all fields",
+      });
+    }
 
-userRouter.get("/purchases", function(req, res) {
-    res.json({
-        message: "signup endpoint"
-    })
-})
+    const existingUser = await user.findOne({ email });
+    if (existingUser) {
+      return res.status(400).json({
+        message: "User already exists",
+      });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const newUser = new user({
+      email,
+      password: hashedPassword,
+      firstName,
+      lastName,
+    });
+
+    await newUser.save();
+
+    return res.json({
+      message: "signed up successfully",
+    });
+  } catch (err) {
+    console.log(err);
+    return res.status(400).json({
+      message: "Something went wrong !!!",
+    });
+  }
+});
+
+userRouter.post("/signin", async function (req, res) {
+  try {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+      return res.status(400).json({
+        message: "Please provide all fields",
+      });
+    }
+
+    const existingUser = await user.findOne({ email: email });
+
+    if (!existingUser) {
+      return res.status(400).json({
+        message: "User does not exist",
+      });
+    }
+
+    const isPasswordCorrect = await bcrypt.compare(
+      password,
+      existingUser.password
+    );
+
+    if (!isPasswordCorrect) {
+      return res.status(400).json({
+        message: "Incorrect password",
+      });
+    }
+
+    const token = jwt.sign(
+      {
+        id: existingUser._id,
+        email: existingUser.email,
+        firstName: existingUser.firstName,
+        lastName: existingUser.lastName,
+      },
+      process.env.JWT_USER_PASSWORD
+    );
+
+    return res.json({
+      message: "signed in successfully",
+      token: token,
+    });
+  } catch (err) {
+    console.log(err);
+    return res.status(400).json({
+      message: "Something went wrong !!!",
+    });
+  }
+});
+
+userRouter.get("allcourse", async function (req, res) {
+  res.json({
+    message: "all courses mentioned here ",
+  });
+});
+
+userRouter.get("purchase", userAuth, async function (req, res) {
+  res.json({
+    message: "You are purchasing the course",
+  });
+});
+
+userRouter.get("/purchases", userAuth, async function (req, res) {
+  res.json({
+    message: "All the purchased courses",
+  });
+});
 
 module.exports = {
-    userRouter: userRouter
-}
+  userRouter: userRouter,
+};
