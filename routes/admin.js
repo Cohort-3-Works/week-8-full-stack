683ff42b53087cc9c963ff84ff682d0a45d69ac9
Subhajit Chaudhury <subhajitchaudhury05@gmail.com>
2024-09-26 02:30:56 +0530

    added sigin and signup for admin

--- routes/admin.js
+++ routes/admin.js
@@ -1,36 +1,103 @@
 const { Router } = require("express");
+const { adminAuth } = require("../middleware/admin");
+const JWT_ADMIN_PASSWORD = require("../config");
+const bcrypt = require("bcrypt");
 const adminRouter = Router();
 
-adminRouter.post("/signup", function(req, res) {
-    res.json({
-        message: "signup endpoint"
-    })
-})
+adminRouter.post("/signup", async function (req, res) {
+  const { email, password, firstName, lastName } = req.body;
 
-adminRouter.post("/signin", function(req, res) {
-    res.json({
-        message: "signup endpoint"
-    })
-})
+  if (!email || !password || !firstName || !lastName) {
+    return res.status(400).json({
+      message: "Please provide all fields",
+    });
+  }
 
-adminRouter.post("/course", function(req, res) {
-    res.json({
-        message: "signup endpoint"
-    })
-})
+  const existingUser = await admin.findOne({ email });
+  if (existingUser) {
+    return res.status(400).json({
+      message: "Admin already exists try using a different mail",
+    });
+  }
 
-adminRouter.put("/course", function(req, res) {
-    res.json({
-        message: "signup endpoint"
-    })
-})
+  const hashedPassword = await bcrypt.hash(password, 10);
 
-adminRouter.get("/course/bulk", function(req, res) {
-    res.json({
-        message: "signup endpoint"
-    })
-})
+  const newAdmin = new admin({
+    email,
+    password: hashedPassword,
+    firstName,
+    lastName,
+  });
+
+  await newAdmin.save();
+
+  return res.json({
+    success: true,
+    message: "Admin created successfully",
+  });
+});
+
+adminRouter.post("/signin", async function (req, res) {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    return res.status(400).json({
+      message: "Please provide all fields",
+    });
+  }
+  const existingUser = await admin.findOne({ email: email });
+
+  if (!existingUser) {
+    return res.status(400).json({
+      message: "Admin does not exist",
+    });
+  }
+
+  const isPasswordCorrect = await bcrypt.compare(
+    password,
+    existingUser.password
+  );
+  if (!isPasswordCorrect) {
+    return res.status(400).json({
+      message: "Incorrect password",
+    });
+  }
+
+  const token = jwt.sign(
+    {
+      id: existingUser._id,
+      email: existingUser.email,
+      firstName: existingUser.firstName,
+      lastName: existingUser.lastName,
+    },
+    JWT_ADMIN_PASSWORD
+  );
+
+  return res.json({
+    success: true,
+    message: "signed in successfully",
+    token,
+  });
+});
+
+adminRouter.post("/createcourse", function (req, res) {
+  res.json({
+    message: "signup endpoint",
+  });
+});
+
+adminRouter.put("/deletecourse", function (req, res) {
+  res.json({
+    message: "signup endpoint",
+  });
+});
+
+adminRouter.get("/course/bulk", function (req, res) {
+  res.json({
+    message: "signup endpoint",
+  });
+});
 
 module.exports = {
-    adminRouter: adminRouter
-}
+  adminRouter: adminRouter,
+};
